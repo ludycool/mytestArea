d1b66f2786479fe5bb0a6aec70d8495831b102a8
ludycool <ludycool@f79b6532-a0db-43d0-9bc6-33d7451f3697>
2016-06-22 07:48:17 +0000

    在配置文件里配置

--- trunk/wcftest/Client/Program.cs
+++ trunk/wcftest/Client/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Client.ServiceReference1;
+using mywcfService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +13,25 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            using (CalculatorServiceClient proxy = new CalculatorServiceClient())
+            //using (CalculatorServiceClient proxy = new CalculatorServiceClient())
+            //{
+            //    Console.WriteLine("x+y={2} when x={0} and y={1}", 1, 2, proxy.Add(1, 2));
+            //    Console.WriteLine("x-y={2} when x={0} and y={1}", 10, 2, proxy.Subtract(10, 2));
+            //    Console.WriteLine("x*y={2} when x={0} and y={1}", 10, 2, proxy.Multiply(10, 2));
+            //    Console.WriteLine("x/y={2} when x={0} and y={1}", 10, 2, proxy.Divide(10, 2));
+            //    Console.Read();
+            //}
+            using (ChannelFactory<ICalculator> channelFactory = new ChannelFactory<ICalculator>("CalculatorService"))
             {
+                ICalculator proxy = channelFactory.CreateChannel();
                 Console.WriteLine("x+y={2} when x={0} and y={1}", 1, 2, proxy.Add(1, 2));
                 Console.WriteLine("x-y={2} when x={0} and y={1}", 10, 2, proxy.Subtract(10, 2));
                 Console.WriteLine("x*y={2} when x={0} and y={1}", 10, 2, proxy.Multiply(10, 2));
                 Console.WriteLine("x/y={2} when x={0} and y={1}", 10, 2, proxy.Divide(10, 2));
-                Console.Read();
+                Console.ReadKey();
             }
+
+
         }
     }
 }
--- trunk/wcftest/Hosting/Program.cs
+++ trunk/wcftest/Hosting/Program.cs
@@ -13,28 +13,19 @@ namespace Hosting
     {
         static void Main(string[] args)
         {
-            using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
-            {
-                host.AddServiceEndpoint(
-                    typeof(ICalculator),
-                    new WSHttpBinding(),
-                    "http://127.0.0.1:1111/CalculatorService");
-                if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
-                {
-                    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
-                    behavior.HttpGetEnabled = true;
-                    behavior.HttpGetUrl = new Uri("http://127.0.0.1:1111/CalculatorService/metadata");
-                    host.Description.Behaviors.Add(behavior);
-                }
-                host.Opened += delegate
-                {
-                    Console.Write("CalculatorService已经启动，按任意键终止服务");
-                };
-                host.Open();
-                Console.Read();
-            }
-            //using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))//已经在app.config配置
+            //using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
             //{
+            //    host.AddServiceEndpoint(
+            //        typeof(ICalculator),
+            //        new WSHttpBinding(),
+            //        "http://127.0.0.1:1111/CalculatorService");
+            //    if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+            //    {
+            //        ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+            //        behavior.HttpGetEnabled = true;
+            //        behavior.HttpGetUrl = new Uri("http://127.0.0.1:1111/CalculatorService/metadata");
+            //        host.Description.Behaviors.Add(behavior);
+            //    }
             //    host.Opened += delegate
             //    {
             //        Console.Write("CalculatorService已经启动，按任意键终止服务");
@@ -42,6 +33,15 @@ namespace Hosting
             //    host.Open();
             //    Console.Read();
             //}
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))//已经在app.config配置
+            {
+                host.Opened += delegate
+                {
+                    Console.Write("CalculatorService已经启动，按任意键终止服务");
+                };
+                host.Open();
+                Console.Read();
+            }
         }
     }
 }
