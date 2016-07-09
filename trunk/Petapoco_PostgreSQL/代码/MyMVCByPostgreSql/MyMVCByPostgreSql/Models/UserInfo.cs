beab02be20442c9b339889bd6e803afe9fc64f79
ludycool <ludycool@f79b6532-a0db-43d0-9bc6-33d7451f3697>
2016-07-09 09:19:12 +0000

    PetaPoco postgresql

---
+++ trunk/Petapoco_PostgreSQL/代码/MyMVCByPostgreSql/MyMVCByPostgreSql/Models/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetaPoco
+{
+    [TableName("userinfo")]
+    [PrimaryKey("id")]
+    [ExplicitColumns]
+    public class UserInfo
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("age")]
+        public int Age { get; set; }
+
+        [Column("qq")]
+        public int Qq { get; set; }
+    }
+}
