785b770b5512ed4ac0b98df7326929d14eecf37a
ludycool <ludycool@f79b6532-a0db-43d0-9bc6-33d7451f3697>
2016-08-24 03:20:40 +0000

    原来是网络问题

--- wcftest/Client/Program.cs
+++ wcftest/Client/Program.cs
@@ -13,33 +13,30 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            using (wcfSendServerClient proxy = new Client.ServiceReference1.wcfSendServerClient())
-            {
-
-                Console.Read();
-                while (true)
-                {
-                    string data = Console.ReadLine();
-                    string ip = "127.0.0.1";
-                    int pot = 9999;
-
-                 bool dd=   proxy.UdpSendData4(data, ip, pot);
-
-                 Console.Write(dd);
-                }
-
-            }
-            //using (ChannelFactory<ICalculator> channelFactory = new ChannelFactory<ICalculator>("CalculatorService"))
+            //using (wcfSendServerClient proxy = new Client.ServiceReference1.wcfSendServerClient())
             //{
-            //    ICalculator proxy = channelFactory.CreateChannel();
-            //    Console.WriteLine("x+y={2} when x={0} and y={1}", 1, 2, proxy.Add(1, 2));
-            //    Console.WriteLine("x-y={2} when x={0} and y={1}", 10, 2, proxy.Subtract(10, 2));
-            //    Console.WriteLine("x*y={2} when x={0} and y={1}", 10, 2, proxy.Multiply(10, 2));
-            //    Console.WriteLine("x/y={2} when x={0} and y={1}", 10, 2, proxy.Divide(10, 2));
-            //    Console.ReadKey();
+
+            //    Console.Read();
+            //    while (true)
+            //    {
+            //        string data = Console.ReadLine();
+            //        string ip = "127.0.0.1";
+            //        int pot = 9999;
+
+            //     bool dd=   proxy.UdpSendData4(data, ip, pot);
+
+            //     Console.Write(dd);
+            //    }
             //}
-
-
+            using (ChannelFactory<ICalculator> channelFactory = new ChannelFactory<ICalculator>("CalculatorService2"))
+            {
+                ICalculator proxy = channelFactory.CreateChannel();
+                Console.WriteLine("x+y={2} when x={0} and y={1}", 1, 2, proxy.Add(1, 2));
+                Console.WriteLine("x-y={2} when x={0} and y={1}", 10, 2, proxy.Subtract(10, 2));
+                Console.WriteLine("x*y={2} when x={0} and y={1}", 10, 2, proxy.Multiply(10, 2));
+                Console.WriteLine("x/y={2} when x={0} and y={1}", 10, 2, proxy.Divide(10, 2));
+                Console.ReadKey();
+            }
         }
     }
 }
